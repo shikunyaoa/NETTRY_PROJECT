f4220becd59ac3684708d02dd87275c1804be3ee
sky <2439677061.com>
2021-02-23 21:33:55 +0800

    基于netty的HTTP的文件服务器

---
+++ src/main/java/com/sky/netty/http/HttpFileServer.java
@@ -0,0 +1,69 @@
+package com.sky.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import sun.reflect.generics.scope.Scope;
+
+/**
+ * @author sky
+ * @description: 基于netty的http应用程序
+ * @date: 2021-02-23
+ */
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/src/com/sky/netty";
+
+    public void run(final int port, final String url) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            //将多个消息转化为单个FullHttpRequest或者FullHttpResponse
+                            socketChannel.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65535));
+                            socketChannel.pipeline().addLast("http-encoder", new HttpRequestEncoder());
+                            //支持发送大的码流
+                            socketChannel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            socketChannel.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                        }
+                    });
+            ChannelFuture future = b.bind("192.168.3.15", port).sync();
+            System.out.println("HTTP文件目录服务器启动，地址是 ：http://192.168.3.15:" + port + url);
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if(args != null && args.length > 0){
+            try{
+                port = Integer.valueOf(args[0]);
+            }catch(NumberFormatException e){
+
+            }
+        }
+        String url = DEFAULT_URL;
+        if(args.length > 1){
+            url = args[1];
+        }
+        new HttpFileServer().run(port, url);
+    }
+}
---
+++ src/main/java/com/sky/netty/messagepack/MsgpackDecoder.java
@@ -0,0 +1,26 @@
+package com.sky.netty.messagepack;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import org.msgpack.MessagePack;
+
+import java.util.List;
+
+/**
+ * @author sky
+ * @description: 基于Messagepack的解码器
+ * @date: 2021-02-23
+ */
+public class MsgpackDecoder extends MessageToMessageDecoder<ByteBuf> {
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        final byte[] array;
+        final int length = byteBuf.readableBytes();
+        array = new byte[length];
+        byteBuf.getBytes(byteBuf.readerIndex(), array, 0, length);
+        MessagePack messagePack = new MessagePack();
+        list.add(messagePack.read(array));
+    }
+}
